bb834a3eec01cfd265449d31bcf60e9e6f9379ca
aurem_pifarre <axp414@francis.edu>
2024-01-23 20:47:26 -0500

    pearl file updated

--- sudo.pl
+++ sudo.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/perl
 
-use strict
-use warnings
+use strict;
+use warnings;
 
+#using the journalctl command to find how many sudo sessions and counting them
 my $journal = 'journalctl _SYSTEMD_UNIT=sudo.service | grep "COMMAND=/usr/bin/sudo" | grep "sudo:session" ';
-my $result = 'journal_cmd';
+my $result = '$journal';
+
+my @split_lines = split(/\n/, $result); #splitting the lines
+my $counter = scalar @split_lines;	#counting how many lines
+
+print "$count\n";
 
 
