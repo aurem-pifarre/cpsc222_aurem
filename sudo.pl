dfd3a676a10e07c415e354ca379fe319bcf7b61b
aurem_pifarre <axp414@francis.edu>
2024-01-23 20:24:40 -0500

    adding the journalctl command

--- sudo.pl
+++ sudo.pl
@@ -1,2 +1,9 @@
 #!/usr/bin/perl
 
+use strict
+use warnings
+
+my $journal = 'journalctl _SYSTEMD_UNIT=sudo.service | grep "COMMAND=/usr/bin/sudo" | grep "sudo:session" ';
+my $result = 'journal_cmd';
+
+
