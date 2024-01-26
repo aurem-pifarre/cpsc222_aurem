4514fbf1cf6ae692b9414f81bf5431469713884b
aurem_pifarre <axp414@francis.edu>
2024-01-25 22:36:53 -0500

    perl file

--- sudo.pl
+++ sudo.pl
@@ -6,8 +6,8 @@ use warnings;
 my $count = 0;
 open(my $file, '<', '/var/log/auth.log') or die $!;
 while (my $line = <$file>){
-	if ($line =~ /sudo:sessions/ && $line =~/opened/) 
-	scalar $count++;
+	if ($line =~ m/sudo:session/ && $line =~ m/opened/){ 
+	 $count++;
 	}
 }
 close($file);
