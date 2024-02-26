d955886e7ebd7989e0807e04539b7b05055b7f21
aurem_pifarre <axp414@francis.edu>
2024-02-26 16:29:47 -0500

    python api

--- python_api.py
+++ python_api.py
@@ -10,20 +10,19 @@ auth = ("test", "abcABC123")
 @app.route('/api/users', methods=["POST"])
 def users():
 	if request.authorization and request.authorization.username == auth[0] and request.authorization.password == auth[1]:
-		users = getpass.getusers()
-		return jsonify({"users": users})
+		all_users = [user.pw_name for user in pwd.getpwall()]
+		return jsonify({"users": all_users})
 	else:
 		return "unauthorized", 401
 
 @app.route('/api/groups', methods=["POST"])
 def groups():
 	if request.authorization and request.authorization.username == auth[0] and request.authorization.password == auth[1]:
-		groups = getpass.getgroups()
-		return jsonify({"groups": groups})
+		all_groups = [group.gr_name for group in grp.getgrall()]
+		return jsonify({"groups": all_groups})
 	else:
 		return "unauthorized", 401
-	
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=3000)
+	app.run(host='localhost', port=3000)
 	
 	
