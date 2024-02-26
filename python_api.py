d4aeb188866ec7cb9bdd5fa34fcbed16461cb804
aurem_pifarre <axp414@francis.edu>
2024-02-26 09:22:20 -0500

    python api

--- python_api.py
+++ python_api.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, request, jsonify
+import getpass
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-	return 'Hello, world!'
+auth = ("test", "abcABC123")
+
+@app.route('/api/users', methods=["POST"])
+def users():
+	if request.authorization and request.authorization.username = auth[0] and request.authorization.password = auth[1]:
+	users = getpass.getuser()
+	return jsonify({"users": users})
+	else:
+	return "unauthorized", 401
+
+@app.route('/api/groups', methods=["POST"])
+def groups():
+	if request.authorization and request.authorization.username = auth[0] and request.authorization.password = auth[1]:
+	groups = getpass.getgroups()
+	return jsonify({"groups": groups})
+	else:
+	return "unauthorized", 401
 	
 if __name__ == '__main__':
-	app.run(host='10.0.2.15', port=3000)
+	app.run(host='0.0.0.0', port=3000)
 	
 	
